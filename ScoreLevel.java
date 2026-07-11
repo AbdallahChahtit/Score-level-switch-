c582aa6d598da4787dbfa17c28e74eb841df8310
Dani <gabood012@gmail.com>
2026-07-11 13:35:57 +0800

    Add files via upload

---
+++ ScoreLevel.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ScoreLevel {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int w = input.nextInt();
+
+        switch (w){
+                     //   case 10:
+                        case 90:
+                            System.out.println("The score level is A");
+                            break;
+                        case 80:
+                            System.out.println("The score level is B");
+                            break;
+                        case 70:
+                            System.out.println("The score level is C");
+                            break;
+                        case 60:
+                            System.out.println("The score level is D");
+                            break;
+                        default:
+                            System.out.println("The score level is F");
+                    }
+    }
+}
+
+
